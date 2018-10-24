9a122dd7615341650e7007e4ef8e2e1852be597e
eugencutic <eugen_cutic@yahoo.com>
2018-10-24 15:49:10 +0300

    tried events and js style

--- ImageGallery/GalleryScript.js
+++ ImageGallery/GalleryScript.js
@@ -1,10 +1,23 @@
-function myFunc()
-{
-    var SGImg = document.getElementById("SGImg");
-    SGImg.src = "explorer.jpg";
-}
-
 window.onload = function()
 {
-    document.getElementById("SGImg").onclick=myFunc;
+    var SGImg = document.getElementById("SGImg");
+    SGImg.onclick = function()
+    {
+        SGImg.src = "explorer.jpg";
+        SGImg.onclick = function()
+        {
+            SGImg.src = "SG.jpg";
+        }
+    };
+    SGImg.onmouseover = function()
+    {
+        SGImg.style.width = "98%";
+        SGImg.style.border = "solid 2px green";
+        SGImg.style.borderRadius = "10px";
+    }
+    SGImg.onmouseleave = function()
+    {
+        SGImg.style.width = "100%";
+        SGImg.style.border = "none";
+    }
 }
