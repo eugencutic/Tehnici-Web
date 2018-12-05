19ca398b9ecdf87ed7f0b37396da6d61885fe6d6
Eugen Cutic <eugen_cutic@yahoo.com>
2018-12-05 15:45:36 +0200

    clock with interval and date

--- Tutorials/TimeoutAndInterval/timeoutAndInterval.js
+++ Tutorials/TimeoutAndInterval/timeoutAndInterval.js
@@ -1,21 +1,39 @@
 window.onload = function()
 {
-    buttons = document.querySelectorAll("button");
+    moveButtonsDown();
+    showDateTime();
+}
+
+function showDateTime()
+{
+    let paras = document.querySelectorAll("p");
+    let p = paras[0];
+    setInterval(function(){
+        //let dt = new Date();
+        p.innerHTML = (new Date()).toTimeString();
+        //alert(dt.toDateString());
+    }, 1000);
+}
+
+function moveButtonsDown()
+{
+    var buttons = document.querySelectorAll("button");
     
     for(let b of buttons) {
         b.topNumericalVal = 0;
     }
 
     for(let b of buttons) {
-        b.onclick = function() {
+        b.onclick = function moveDown() {
 
-            var moveDownInterval = setInterval(function() {
+            b.moveDownInterval = setInterval(function() {
                 b.topNumericalVal += 10;
                 b.style.top = b.topNumericalVal + "px";
             }, 100);
 
-            b.onclick = function() {
-                clearInterval(moveDownInterval);
+            b.onclick = function stop() {
+                clearInterval(b.moveDownInterval);
+                b.onclick = moveDown;
             }
 
         }
